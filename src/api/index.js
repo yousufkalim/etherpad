b5309bcb0e2b7257be36e8f207ddc3ed2a48abb8
Yousuf Kalim <yousuf@falconit.com.pk>
2021-12-03 12:34:11 +0500

    Boilerplate updated

--- src/api/index.js
+++ src/api/index.js
@@ -3,13 +3,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = async (method = "get", uri, body) => {
-  // Default setting for production
-  if (window.location.hostname !== "localhost") {
-    axios.defaults.withCredentials = true;
-  }
-
   // API Call
-  const url = process.env.BASE_URL + uri;
+  const url = process.env.REACT_APP_SERVER_URL + uri;
   return new Promise((resolve, reject) => {
     axios[method](url, body)
       .then((res) => resolve(res))
@@ -17,7 +12,7 @@ const api = async (method = "get", uri, body) => {
         if (err?.response?.status === 403) {
           window.location = "/login";
         } else {
-          console.log("API Error --------> ", err.response.status);
+          console.log("API Error --------> ", err?.response?.status);
           toast.error(err?.response?.data ? err.response.data : err?.message);
           reject(err);
         }
