d81e0b90c7aa7bbf2c55f0f12c392a09221a64c2
Yousuf Kalim <yousuf@falconit.com.pk>
2022-09-25 11:21:23 +0500

    Element editor right sidebar

---
+++ src/components/ElementEditor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ElementEditor() {
+  return (
+    <div className="elm_editor">
+      <div className="elm_editor_container">
+        <div className="elm_editor_header"></div>
+      </div>
+    </div>
+  );
+}
+
+export default ElementEditor;
